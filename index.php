3c380ef8e0494fe2d26b660f9675f06d0cb19364
Jed Dockery <35934308+JedDockery@users.noreply.github.com>
2019-04-20 11:13:56 -0400

    Add files via upload
    added javascript file if needed. index.php was modified.

--- index.php
+++ index.php
@@ -1,3 +1,21 @@
+<!doctype html>
 <?php
-echo ("hi there.");
+	include 'project.jeddockery.com/dbh.php'
 ?>
+<html>
+	<head>
+		<meta charset="utf-8">
+		<title>CSCI 3000 final project</title>
+		<link href="breakpoints.css" rel="stylesheet" type="text/css" />
+		<script src="script.js"></script>
+	</head>
+	
+	<body>
+		<script>
+		popup();
+		</script>
+	</body>
+	<?php
+		echo ("hi there.");
+	?>
+</html>
